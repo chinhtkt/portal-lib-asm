6bc5141f280b9687d6d6b064d9a7a46b11678180
chinhtkt <47823938+chinhtkt@users.noreply.github.com>
2021-04-29 11:54:44 +0700

    updated

--- front-end/libary-asm/src/Components/table/CategoryDetails.jsx
+++ front-end/libary-asm/src/Components/table/CategoryDetails.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import {  useParams, Redirect } from 'react-router-dom';
 function CategoryDetails({ categories, setCategories, error, authorities }) {
      const {id} = useParams();
-
      if(!authorities) {
       return <Redirect to ="/home" />
     }
@@ -18,7 +17,7 @@ function CategoryDetails({ categories, setCategories, error, authorities }) {
     //     console.log(result);
     //     setCategories(result.data)
     // }
-    console.log(categories)
+    // console.log(categories)
   return (
     <div>
       {categories &&
@@ -28,7 +27,7 @@ function CategoryDetails({ categories, setCategories, error, authorities }) {
             {category.categoryId == id &&<ListGroup >
             <ListGroup.Item ><h4><strong>ID:</strong>  {category.categoryId}</h4></ListGroup.Item>
             <ListGroup.Item><h4><strong>Name:</strong> {category.name}</h4></ListGroup.Item>
-            <ListGroup.Item><h4><strong>Book:</strong> {category.books.length <= 0 ? <h4>No book found!</h4> : <h3>{category.books.name}</h3>}</h4></ListGroup.Item>
+            <ListGroup.Item><h4><strong>Book:</strong> {category.books.length <= 0 ? <h4>No book found!</h4> : <h3>{category.books.map(x=>x.name).join(', ')}</h3>}</h4></ListGroup.Item>
             <Link to={'/admin'}>
             <br></br>
           <Button>
